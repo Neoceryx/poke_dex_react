effc22a6756496e8ef2f970131efb16c3ebe6025
neoceryx <dani-deoz@hotmail.com>
2024-03-06 15:49:01 -0800

    pokemon attributes to display was added

--- src/pages/listado.tsx
+++ src/pages/listado.tsx
@@ -1,6 +1,7 @@
 import react from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 
 const Listado = () => {
@@ -14,24 +15,33 @@ const Listado = () => {
 
                         <Card className="mx-auto" style={{ width: '18rem' }}>
                             <Card.Header>Tipo:</Card.Header>
-                            <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18e159f0cde%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18e159f0cde%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.19140625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
+                            <Card.Img variant="top" width="80" height="100" className="d-block mx-auto w-50" src="https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif" />
                             <Card.Body>
                                 <Card.Title className="text-center">Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>HP: ##</ListGroup.Item>
+                                    <ListGroup.Item>Ataque: ##</ListGroup.Item>
+                                    <ListGroup.Item>Defensa: ##</ListGroup.Item>
+                                    <ListGroup.Item>E. Ataque: ##</ListGroup.Item>
+                                    <ListGroup.Item>E. Defensa: ##</ListGroup.Item>
+                                    <ListGroup.Item>Velocidad: ##</ListGroup.Item>
+                                </ListGroup>
                             </Card.Body>
                         </Card>
 
                         <Card className="mx-auto" style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18e159f0cde%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18e159f0cde%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.19140625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
+                            <Card.Header>Tipo:</Card.Header>
+                            <Card.Img variant="top" width="80" height="100" className="d-block mx-auto w-50" src="https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif" />
                             <Card.Body>
                                 <Card.Title className="text-center">Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>HP: ##</ListGroup.Item>
+                                    <ListGroup.Item>Ataque: ##</ListGroup.Item>
+                                    <ListGroup.Item>Defensa: ##</ListGroup.Item>
+                                    <ListGroup.Item>E. Ataque: ##</ListGroup.Item>
+                                    <ListGroup.Item>E. Defensa: ##</ListGroup.Item>
+                                    <ListGroup.Item>Velocidad: ##</ListGroup.Item>
+                                </ListGroup>
                             </Card.Body>
                         </Card>
 

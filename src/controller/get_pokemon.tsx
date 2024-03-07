928a583afc327de03a1c74f27ea50b4df5da304a
xion93 <dani-deoz@hotmail.com>
2024-03-06 20:11:03 -0800

    pokemon list was displayed in DOM

--- src/controller/get_pokemon.tsx
+++ src/controller/get_pokemon.tsx
@@ -4,21 +4,21 @@ export async function get_pokemons(): Promise<Pokemon[]> {
     const response = await fetch("https://unpkg.com/pokemons@1.1.0/pokemons.json");
 
     const data = await response.json();
-    const pokemons = data.results.map((pokemon: any) => {
-        id:pokemon.national_number
-        name:pokemon.name
-        img: pokemon.sprites["normal"]
-        img_large: pokemon.sprites["large"]
-        img_gif: pokemon.sprites["animated"]
-        hp: pokemon.hp
-        attack: pokemon.attack
-        defense: pokemon.defense
-        sp_atk: pokemon.sp_atk
-        sp_def: pokemon.sp_def
-        speed: pokemon.speed
-    })
-    
+    const pokemons = data.results.map((p: any) => ({
+        id: p.national_number,
+        name: p.name,
+        img: p.sprites['normal'], 
+        img_large: p.sprites['large'],  
+        img_gif:p.sprites['animated'],
+        total: p.total,
+        hp: p.hp,
+        attack: p.attack,
+        defense: p.defense,
+        sp_atk: p.sp_atk,
+        sp_def: p.sp_def,
+        speed: p.speed,
+        type: p.type[0]
+    }));
 
-
-    return pokemons
+    return pokemons    
 }
--- src/pages/listado.tsx
+++ src/pages/listado.tsx
@@ -1,10 +1,22 @@
-import react from "react";
+import react, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+import { get_pokemons } from "../controller/get_pokemon";
+import { Pokemon } from "../models/pokemon_m";
 
 const Listado = () => {
+
+    const[pokemons, setPokemon] = useState<Pokemon[]>([])
+
+    useEffect(() => {
+        const get_all_pokemons = async() => {
+            setPokemon(await get_pokemons())
+        }
+        get_all_pokemons();
+    });
+
     return (
         <>
             <h1>Pokemon's</h1>
@@ -13,37 +25,23 @@ const Listado = () => {
                 <div className="content">
                     <div className="row gap-3">
 
-                        <Card className="mx-auto" style={{ width: '18rem' }}>
-                            <Card.Header>Tipo:</Card.Header>
-                            <Card.Img variant="top" width="80" height="100" className="d-block mx-auto w-50" src="https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif" />
-                            <Card.Body>
-                                <Card.Title className="text-center">Card Title</Card.Title>
-                                <ListGroup variant="flush">
-                                    <ListGroup.Item>HP: ##</ListGroup.Item>
-                                    <ListGroup.Item>Ataque: ##</ListGroup.Item>
-                                    <ListGroup.Item>Defensa: ##</ListGroup.Item>
-                                    <ListGroup.Item>E. Ataque: ##</ListGroup.Item>
-                                    <ListGroup.Item>E. Defensa: ##</ListGroup.Item>
-                                    <ListGroup.Item>Velocidad: ##</ListGroup.Item>
-                                </ListGroup>
-                            </Card.Body>
-                        </Card>
-
-                        <Card className="mx-auto" style={{ width: '18rem' }}>
-                            <Card.Header>Tipo:</Card.Header>
-                            <Card.Img variant="top" width="80" height="100" className="d-block mx-auto w-50" src="https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif" />
-                            <Card.Body>
-                                <Card.Title className="text-center">Card Title</Card.Title>
-                                <ListGroup variant="flush">
-                                    <ListGroup.Item>HP: ##</ListGroup.Item>
-                                    <ListGroup.Item>Ataque: ##</ListGroup.Item>
-                                    <ListGroup.Item>Defensa: ##</ListGroup.Item>
-                                    <ListGroup.Item>E. Ataque: ##</ListGroup.Item>
-                                    <ListGroup.Item>E. Defensa: ##</ListGroup.Item>
-                                    <ListGroup.Item>Velocidad: ##</ListGroup.Item>
-                                </ListGroup>
-                            </Card.Body>
-                        </Card>
+                        {pokemons?.slice(0, 12).map((p, index) => (
+                            <Card key={index} className="mx-auto" style={{ width: '18rem' }}>
+                                <Card.Header>Tipo:{p.type}</Card.Header>
+                                <Card.Img variant="top" width="80" height="100" className="d-block mx-auto w-50" src={p.img} />
+                                <Card.Body>
+                                    <Card.Title className="text-center">{p.name}</Card.Title>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item>HP: {p.hp}</ListGroup.Item>
+                                        <ListGroup.Item>Ataque: {p.attack}</ListGroup.Item>
+                                        <ListGroup.Item>Defensa: {p.defense}</ListGroup.Item>
+                                        <ListGroup.Item>E. Ataque: {p.sp_atk}</ListGroup.Item>
+                                        <ListGroup.Item>E. Defensa: {p.sp_def}</ListGroup.Item>
+                                        <ListGroup.Item>Velocidad: {p.speed}</ListGroup.Item>
+                                    </ListGroup>
+                                </Card.Body>
+                            </Card>
+                        ))}
 
                     </div>
                 </div>
@@ -51,6 +49,7 @@ const Listado = () => {
 
         </>
     )
+
 }
 
 export default Listado;
